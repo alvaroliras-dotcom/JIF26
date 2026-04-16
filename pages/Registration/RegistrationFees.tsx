45b22baa58df8b81eda4a5d3a6a760e6ab01cc87
alvaroliras <alvaroliras@gmail.com>
2026-04-16 14:26:32 +0200

    test visible change

--- pages/Registration/RegistrationFees.tsx
+++ pages/Registration/RegistrationFees.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RegistrationFees: React.FC = () => {
 return ( <div> <a href="https://app.scitoevents.com/JIF26/home" target="_blank" rel="noopener noreferrer">
-Proceed to payment </a> </div>
+ESTO ES UNA PUTA PRUEBA </a> </div>
 );
 };
 
