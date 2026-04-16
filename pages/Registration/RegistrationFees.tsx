3541dda4e0c9733bfd9eb6cf7efa1025b8d471cd
alvaroliras <alvaroliras@gmail.com>
2026-04-16 14:39:50 +0200

    clean node_modules fix

--- pages/Registration/RegistrationFees.tsx
+++ pages/Registration/RegistrationFees.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const RegistrationFees: React.FC = () => {
-return ( <div> <a href="https://app.scitoevents.com/JIF26/home" target="_blank" rel="noopener noreferrer">
-ESTO ES UNA PUTA PRUEBA </a> </div>
-);
+return <h1>TEST TOTAL</h1>;
 };
 
 export default RegistrationFees;
